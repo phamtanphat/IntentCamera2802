6b4157f88ce9656c2ac2f0333ac060262a489a4f
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-25 18:14:14 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/intentcamera/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/intentcamera/MainActivity.java
@@ -1,13 +1,26 @@
 package khoapham.ptp.phamtanphat.intentcamera;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import khoapham.ptp.phamtanphat.intentcamera.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(MainActivity.this,R.layout.activity_main);
+
+        activityMainBinding.buttonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button camera", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
