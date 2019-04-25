15f7b75bac23dfd021fac8499cfb0f5f1b0043a1
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-25 18:40:56 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/intentcamera/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/intentcamera/MainActivity.java
@@ -1,6 +1,15 @@
 package khoapham.ptp.phamtanphat.intentcamera;
 
+import android.Manifest;
+import android.app.Application;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
+import android.databinding.Observable;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,19 +17,35 @@ import android.widget.Toast;
 
 import khoapham.ptp.phamtanphat.intentcamera.databinding.ActivityMainBinding;
 
+
 public class MainActivity extends AppCompatActivity {
 
-
+    int Request_Code_Camera = 10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(MainActivity.this,R.layout.activity_main);
+        ActivityMainBinding mainBinding = DataBindingUtil.setContentView(MainActivity.this,R.layout.activity_main);
 
-        activityMainBinding.buttonCamera.setOnClickListener(new View.OnClickListener() {
+        mainBinding.buttonCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Button camera", Toast.LENGTH_SHORT).show();
+                if (ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},
+                            Request_Code_Camera);
+                }
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == Request_Code_Camera){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
